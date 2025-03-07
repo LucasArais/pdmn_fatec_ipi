c224d9c883f39af9d20eced9380f4c8a2903a412
Lucas Arais <lucas_arais@hotmail.com>
2025-03-07 20:13:28 -0300

    learn(ap04-react): encerra a sessão 2.5
    - vamos comecar a refatoracao criando um componente que representa pedidos

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -4,21 +4,81 @@ const App = () => {
     <div className="row">
       
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <p>Caixa 1</p>
+        
+        <div className="card">
+          <div className="card-header text muted">
+            22/04/2021
+          </div>
+          <div className="card-body d-flex">
+            <div className="d-flex align-items-center">
+            <i class="fa-solid fa-headset fa-2x fa-shake"></i>
+            </div>
+            <div className="border flex-grow-1 ms-3 py-2 ">
+              <h4 className="text-center">Headset</h4>
+              <p className="text-center">Headset Bluetooth</p>
+            </div>
+          </div>
+        </div>
+
+     </div>
+
+      <div className="col-sm-12 col-lg-6 col-xxl-3">
+        
+      <div className="card">
+          <div className="card-header text muted">
+            22/04/2022
+          </div>
+          <div className="card-body d-flex">
+            <div className="d-flex align-items-center">
+            <i class="fa-solid fa-gamepad fa-2x fa-shake"></i>
+            </div>
+            <div className="border flex-grow-1 ms-3 py-2 ">
+              <h4 className="text-center">Gamepad</h4>
+              <p className="text-center">Controle Nintendo</p>
+            </div>
+          </div>
+        </div>
+        
       </div>
 
       <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <p>Caixa 2</p>
-      </div>
-
-      <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <p>Caixa 3</p>
-      </div>
-
-      <div className="col-sm-12 col-lg-6 col-xxl-3">
-        <p>Caixa 4</p>
-      </div>
       
+      <div className="card">
+          <div className="card-header text muted">
+            22/04/2023
+          </div>
+          <div className="card-body d-flex">
+            <div className="d-flex align-items-center">
+            <i class="fa-solid fa-hippo fa-2x fa-shake"></i>
+            </div>
+            <div className="border flex-grow-1 ms-3 py-2 ">
+              <h4 className="text-center">Hipopotamo</h4>
+              <p className="text-center">Hipopotamo femea</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="col-sm-12 col-lg-6 col-xxl-3">
+        
+      <div className="card">
+          <div className="card-header text muted">
+            22/04/2024
+          </div>
+          <div className="card-body d-flex">
+            <div className="d-flex align-items-center">
+            <i class="fa-solid fa-snowman fa-2x fa-shake"></i>
+            </div>
+            <div className="border flex-grow-1 ms-3 py-2 ">
+              <h4 className="text-center">Boneco de Neve</h4>
+              <p className="text-center">Esse boneco de neve fica balançando</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   </div>
   )
