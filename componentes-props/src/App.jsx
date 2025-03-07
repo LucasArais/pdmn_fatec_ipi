8a29c255fdf11a6c2353ac885e6c84d6ee28b749
Lucas Arais <lucas_arais@hotmail.com>
2025-03-07 17:01:30 -0300

    learn(ap04-react): encerra o codigo 2.5.4
    -Fizemos a responsividade para 4 caixas

---
+++ componentes-props/src/App.jsx
@@ -0,0 +1,27 @@
+const App = () => {
+  return (
+  <div className="container border rounder mt-2">
+    <div className="row">
+      
+      <div className="col-sm-12 col-lg-6 col-xxl-3">
+        <p>Caixa 1</p>
+      </div>
+
+      <div className="col-sm-12 col-lg-6 col-xxl-3">
+        <p>Caixa 2</p>
+      </div>
+
+      <div className="col-sm-12 col-lg-6 col-xxl-3">
+        <p>Caixa 3</p>
+      </div>
+
+      <div className="col-sm-12 col-lg-6 col-xxl-3">
+        <p>Caixa 4</p>
+      </div>
+      
+    </div>
+  </div>
+  )
+}
+
+export default App
